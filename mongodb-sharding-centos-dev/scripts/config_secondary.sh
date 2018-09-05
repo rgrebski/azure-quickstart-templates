1e21d6167b12eb3ffea97be8b090746f46835f38
Radek Grebski <radoslaw.grebski@stepwise.pl>
2018-09-05 17:53:21 +0200

    WIP: initial version of bash script for secondary config (+ standalone shard) nodes

--- mongodb-sharding-centos-dev/scripts/config_secondary.sh
+++ mongodb-sharding-centos-dev/scripts/config_secondary.sh
@@ -1,137 +1,227 @@
 #!/bin/bash
 
-#zabbixServer=$1
-#
-#install_mongo3() {
-##create repo
-#cat > /etc/yum.repos.d/mongodb-org-3.2.repo <<EOF
-#[mongodb-org-3.2]
-#name=MongoDB Repository
-#baseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/3.2/x86_64/
-#gpgcheck=0
-#enabled=1
-#EOF
-#
-#	#install
-#	yum install -y mongodb-org
-#
-#	#ignore update
-#	sed -i '$a exclude=mongodb-org,mongodb-org-server,mongodb-org-shell,mongodb-org-mongos,mongodb-org-tools' /etc/yum.conf
-#
-#	#disable selinux
-#	sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/sysconfig/selinux
-#	sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
-#	setenforce 0
-#
-#	#kernel settings
-#	if [[ -f /sys/kernel/mm/transparent_hugepage/enabled ]];then
-#		echo never > /sys/kernel/mm/transparent_hugepage/enabled
-#	fi
-#	if [[ -f /sys/kernel/mm/transparent_hugepage/defrag ]];then
-#		echo never > /sys/kernel/mm/transparent_hugepage/defrag
-#	fi
-#
-#	#configure
-#	sed -i 's/\(bindIp\)/#\1/' /etc/mongod.conf
-#
-#	#set keyfile
-#	echo "vfr4CDE1" > /etc/mongokeyfile
-#	chown mongod:mongod /etc/mongokeyfile
-#	chmod 600 /etc/mongokeyfile
-#	sed -i 's/^#security/security/' /etc/mongod.conf
-#	sed -i '/^security/akeyFile: /etc/mongokeyfile' /etc/mongod.conf
-#	sed -i 's/^keyFile/  keyFile/' /etc/mongod.conf
-#}
-#
-#
-#install_zabbix() {
-#	#install zabbix agent
-#	cd /tmp
-#	yum install -y gcc wget > /dev/null
-#	wget http://jaist.dl.sourceforge.net/project/zabbix/ZABBIX%20Latest%20Stable/2.2.5/zabbix-2.2.5.tar.gz > /dev/null
-#	tar zxvf zabbix-2.2.5.tar.gz
-#	cd zabbix-2.2.5
-#	groupadd zabbix
-#	useradd zabbix -g zabbix -s /sbin/nologin
-#	mkdir -p /usr/local/zabbix
-#	./configure --prefix=/usr/local/zabbix --enable-agent
-#	make install > /dev/null
-#	cp misc/init.d/fedora/core/zabbix_agentd /etc/init.d/
-#	sed -i 's/BASEDIR=\/usr\/local/BASEDIR=\/usr\/local\/zabbix/g' /etc/init.d/zabbix_agentd
-#	sed -i '$azabbix-agent    10050/tcp\nzabbix-agent    10050/udp' /etc/services
-#	sed -i '/^LogFile/s/tmp/var\/log/' /usr/local/zabbix/etc/zabbix_agentd.conf
-#	hostName=`hostname`
-#	sed -i "s/^Hostname=Zabbix server/Hostname=$hostName/" /usr/local/zabbix/etc/zabbix_agentd.conf
-#	if [[ $zabbixServer =~ ([0-9]{1,3}.){3}[0-9]{1,3} ]];then
-#		sed -i "s/^Server=127.0.0.1/Server=$zabbixServer/" /usr/local/zabbix/etc/zabbix_agentd.conf
-#		sed -i "s/^ServerActive=127.0.0.1/ServerActive=$zabbixServer/" /usr/local/zabbix/etc/zabbix_agentd.conf
-#		sed -i "s/^Server=127.0.0.1/Server=$zabbixServer/" /usr/local/zabbix/etc/zabbix_agent.conf
-#	fi
-#	touch /var/log/zabbix_agentd.log
-#	chown zabbix:zabbix /var/log/zabbix_agentd.log
-#
-#	#start zabbix agent
-#	chkconfig --add zabbix_agentd
-#	chkconfig zabbix_agentd on
-#	/etc/init.d/zabbix_agentd start
-#}
-#
-#
-#install_mongo3
-#install_zabbix
-#
-##start config replica set
-#mongod --configsvr --replSet crepset --port 27019 --dbpath /var/lib/mongo/ --logpath /var/log/mongodb/config.log --fork --config /etc/mongod.conf
-#
-#
-#
-#
-##check if mongod started or not
-#sleep 15
-#n=`ps -ef |grep "mongod --configsvr" |grep -v grep |wc -l`
-#if [[ $n -eq 1 ]];then
-#    echo "mongod config replica set started successfully"
-#else
-#    echo "mongod config replica set started failed!"
-#fi
-#
-#
-##set mongod auto start
-#cat > /etc/init.d/mongod1 <<EOF
-##!/bin/bash
-##chkconfig: 35 84 15
-##description: mongod auto start
-#. /etc/init.d/functions
-#
-#Name=mongod1
-#start() {
-#if [[ ! -d /var/run/mongodb ]];then
-#mkdir /var/run/mongodb
-#chown -R mongod:mongod /var/run/mongodb
-#fi
-#mongod --configsvr --replSet crepset --port 27019 --dbpath /var/lib/mongo/ --logpath /var/log/mongodb/config.log --fork --config /etc/mongod.conf
-#}
-#stop() {
-#pkill mongod
-#}
-#restart() {
-#stop
-#sleep 15
-#start
-#}
-#
-#case "\$1" in
-#    start)
-#	start;;
-#	stop)
-#	stop;;
-#	restart)
-#	restart;;
-#	status)
-#	status \$Name;;
-#	*)
-#	echo "Usage: service mongod1 start|stop|restart|status"
-#esac
-#EOF
-#chmod +x /etc/init.d/mongod1
-#chkconfig mongod1 on
+mongoKeyFile='2Rr_Czkjm.!N)cuA'
+
+install_mongo3() {
+    #install mongo3
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+
+    apt update
+    sudo apt install -y mongodb-org
+
+    sudo systemctl stop mongod.service
+
+
+# --------- CONFIG SERVER CONFIG + SERVICE [START] -------------
+
+cat <<EOT > /etc/mongod_configsvr.conf
+# mongod.conf
+
+# for documentation of all options, see:
+#   http://docs.mongodb.org/manual/reference/configuration-options/
+
+# Where and how to store data.
+storage:
+  dbPath: /var/lib/mongodb/configsvr
+  journal:
+    enabled: true
+#  engine:
+#  mmapv1:
+#  wiredTiger:
+
+# where to write logging data.
+systemLog:
+  destination: file
+  logAppend: true
+  path: /var/log/mongodb/mongod_configsvr.log
+
+# network interfaces
+net:
+  port: 27018
+  bindIp: 0.0.0.0
+
+# how the process runs
+processManagement:
+  timeZoneInfo: /usr/share/zoneinfo
+
+replication:
+   replSetName: confReplica
+
+sharding:
+   clusterRole: configsvr
+
+security:
+  keyFile: /etc/mongokeyfile
+EOT
+
+cat <<EOT > /lib/systemd/system/mongod.confsvr.service
+[Unit]
+Description=High-performance, schema-free document-oriented database
+After=network.target
+Documentation=https://docs.mongodb.org/manual
+
+[Service]
+User=mongodb
+Group=mongodb
+ExecStart=/usr/bin/mongod --config /etc/mongod_configsvr.conf
+PIDFile=/var/run/mongodb/mongod.confsvr.pid
+# file size
+LimitFSIZE=infinity
+# cpu time
+LimitCPU=infinity
+# virtual memory size
+LimitAS=infinity
+# open files
+LimitNOFILE=64000
+# processes/threads
+LimitNPROC=64000
+# locked memory
+LimitMEMLOCK=infinity
+# total threads (user+kernel)
+TasksMax=infinity
+TasksAccounting=false
+
+# Recommended limits for for mongod as specified in
+# http://docs.mongodb.org/manual/reference/ulimit/#recommended-settings
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+# --------- CONFIG SERVER CONFIG + SERVICE [END] -------------
+
+# --------- SHARD SERVER CONFIG + SERVICE [START] -------------
+
+cat <<EOT > /etc/mongod_shardsvr.conf
+# mongod.conf
+
+# for documentation of all options, see:
+#   http://docs.mongodb.org/manual/reference/configuration-options/
+
+# Where and how to store data.
+storage:
+  dbPath: /var/lib/mongodb/shardsvr
+  journal:
+    enabled: true
+#  engine:
+#  mmapv1:
+#  wiredTiger:
+
+# where to write logging data.
+systemLog:
+  destination: file
+  logAppend: true
+  path: /var/log/mongodb/mongod_shardsvr.log
+
+# network interfaces
+net:
+  port: 27017
+  bindIp: 0.0.0.0
+
+# how the process runs
+processManagement:
+  timeZoneInfo: /usr/share/zoneinfo
+
+sharding:
+   clusterRole: shardsvr
+
+security:
+  keyFile: /etc/mongokeyfile
+EOT
+
+cat <<EOT > /lib/systemd/system/mongod.shardsvr.service
+[Unit]
+Description=High-performance, schema-free document-oriented database
+After=network.target
+Documentation=https://docs.mongodb.org/manual
+
+[Service]
+User=mongodb
+Group=mongodb
+ExecStart=/usr/bin/mongod --config /etc/mongod_shardsvr.conf
+PIDFile=/var/run/mongodb/mongod.confsvr.pid
+# file size
+LimitFSIZE=infinity
+# cpu time
+LimitCPU=infinity
+# virtual memory size
+LimitAS=infinity
+# open files
+LimitNOFILE=64000
+# processes/threads
+LimitNPROC=64000
+# locked memory
+LimitMEMLOCK=infinity
+# total threads (user+kernel)
+TasksMax=infinity
+TasksAccounting=false
+
+# Recommended limits for for mongod as specified in
+# http://docs.mongodb.org/manual/reference/ulimit/#recommended-settings
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+# --------- SHARD SERVER CONFIG + SERVICE [END] -------------
+
+    chmod 644 /lib/systemd/system/mongod.confsvr.service
+    chmod 644 /lib/systemd/system/mongod.shardsvr.service
+
+    #create dirs
+    mkdir /var/lib/mongodb/configsvr
+    chown mongodb:mongodb /var/lib/mongodb/configsvr/
+    chmod 755 /var/lib/mongodb/configsvr/
+    
+    mkdir /var/lib/mongodb/shardsvr
+    chown mongodb:mongodb /var/lib/mongodb/shardsvr/
+    chmod 755 /var/lib/mongodb/shardsvr/
+
+    #remove default mongo service
+    rm /lib/systemd/system/mongod.service
+
+    #enable autostart
+    systemctl enable mongod.confsvr.service
+    systemctl enable mongod.shardsvr.service
+#------------------------------------------------------
+
+	#kernel settings
+	if [[ -f /sys/kernel/mm/transparent_hugepage/enabled ]];then
+		echo never > /sys/kernel/mm/transparent_hugepage/enabled
+	fi
+	if [[ -f /sys/kernel/mm/transparent_hugepage/defrag ]];then
+		echo never > /sys/kernel/mm/transparent_hugepage/defrag
+	fi
+
+	#set keyfile
+	echo $mongoKeyFile > /etc/mongokeyfile
+	chown mongodb:mongodb /etc/mongokeyfile
+	chmod 600 /etc/mongokeyfile
+}
+
+install_mongo3
+
+    #start config replica set
+    systemctl start mongod.confsvr.service
+    #start shard as standalone server
+    systemctl start mongod.shardsvr.service
+
+
+    #check if mongod started or not
+    sleep 15
+    n=`ps -ef |grep "/usr/bin/mongod --config /etc/mongod_configsvr.conf" |grep -v grep |wc -l`
+    if [[ $n -eq 1 ]];then
+        echo "mongod config replica set started successfully"
+    else
+        echo "mongod config replica set started failed!"
+    fi
+
+    n=`ps -ef |grep "/usr/bin/mongod --config /etc/mongod_shardsvr.conf" |grep -v grep |wc -l`
+    if [[ $n -eq 1 ]];then
+        echo "mongod config replica set started successfully"
+    else
+        echo "mongod config replica set started failed!"
+    fi
+
+
